c6deecc6eb9f91dd9b4795c6f3e23147f40bb16c
James Schlader <jamesschlader@yahoo.com>
2019-12-10 11:24:02 -0700

    Post peer review session

--- src/main/java/com/henryschein/dataservices/csvtranslator/services/CSVTranslatorService.java
+++ src/main/java/com/henryschein/dataservices/csvtranslator/services/CSVTranslatorService.java
@@ -1,24 +1,32 @@
 package com.henryschein.dataservices.csvtranslator.services;
 
+import com.henryschein.dataservices.csvtranslator.interfaces.Translator;
 import com.henryschein.dataservices.csvtranslator.parser.CSVParser;
-import com.henryschein.dataservices.csvtranslator.translator.CSVTranslator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class CSVTranslatorService {
-
     @Autowired
     CSVParser csvParser;
-
     @Autowired
-    CSVTranslator csvTranslator;
+    Translator bracketTranslator;
+    @Autowired
+    Translator reverseTranslator;
 
-    public String translateCSV(String csv) {
+    private static Map<String, Translator> translators = new HashMap<>();
+
+    public String translateCSV(String csv, String type) {
+        translators.putIfAbsent("bracket", bracketTranslator);
+        translators.putIfAbsent("reverse", reverseTranslator);
         List<List<String>> parsedCSV = csvParser.parseCSV(csv);
-        return csvTranslator.translateCSV(parsedCSV);
+        Set<String> keys = translators.keySet();
+        if(!keys.contains(type)) type = "bracket";
+        return translators.get(type).Translate(parsedCSV);
     }
 }
