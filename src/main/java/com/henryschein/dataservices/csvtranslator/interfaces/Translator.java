b66c63997590743fb63df03beb04b0c11d63f5bf
James.Schlader <jamesschlader@yahoo.com>
2019-12-13 12:34:49 -0700

    fully merged versions

--- src/main/java/com/henryschein/dataservices/csvtranslator/interfaces/Translator.java
+++ src/main/java/com/henryschein/dataservices/csvtranslator/interfaces/Translator.java
@@ -1,17 +1,8 @@
 package com.henryschein.dataservices.csvtranslator.interfaces;
 
-<<<<<<< HEAD
 import java.util.List;
 
 public interface Translator {
     public String Translate(List<List<String>> source);
-=======
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-
-public interface Translator {
-    String Translate(List<List<String>> source);
->>>>>>> cd958ae8a78493a37bfa88cbc6386f5e7ddcbf98
 }
+
