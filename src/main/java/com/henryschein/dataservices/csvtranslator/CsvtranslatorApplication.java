c21478e5320e7fdbd1f51c27d875697de7f31f1e
SolitudeDev <SolitudeDev@RDAHL21410.us.hsi.local>
2019-12-13 15:18:40 -0700

    added code to fetch spanish translation in SpanishTranslator

--- src/main/java/com/henryschein/dataservices/csvtranslator/CsvtranslatorApplication.java
+++ src/main/java/com/henryschein/dataservices/csvtranslator/CsvtranslatorApplication.java
@@ -3,6 +3,8 @@ package com.henryschein.dataservices.csvtranslator;
 import com.henryschein.dataservices.csvtranslator.interfaces.Translator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,5 +15,10 @@ public class CsvtranslatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CsvtranslatorApplication.class, args);
     }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    };
 
 }
--- src/main/java/com/henryschein/dataservices/csvtranslator/csvtranslators/SpanishTranslator.java
+++ src/main/java/com/henryschein/dataservices/csvtranslator/csvtranslators/SpanishTranslator.java
@@ -1,14 +1,74 @@
 package com.henryschein.dataservices.csvtranslator.csvtranslators;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.henryschein.dataservices.csvtranslator.interfaces.Translator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
+import java.util.Map;
+
 
 @Component
 public class SpanishTranslator implements Translator {
+    @Autowired
+    RestTemplate restTemplate;
+
     @Override
     public String Translate(List<List<String>> source){
-        return "Gonna translate " + source + " in Spanish!";
+
+        StringBuilder returnString = new StringBuilder();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        source.forEach(list->list.forEach(text-> {
+            String
+                    result = null;
+            try {
+                result = restTemplate.exchange(
+                "https://systran-systran-platform-for-language-processing-v1.p.rapidapi" +
+                        ".com/translation/text/translate?source=en&target=es&input="
+                        + URLEncoder.encode(text, "UTF-8"),
+                HttpMethod.GET, getEntity(), String.class).getBody();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            try{
+                Map<String, List<Map<String, String>>> stringListMap = mapper.readValue(result, Map.class);
+                System.out.println(stringListMap.get("outputs").get(0).get("output"));
+                returnString.append(stringListMap.get("outputs").get(0).get("output"));
+            } catch(IOException error){
+                error.printStackTrace();
+            }
+
+        }));
+
+        return returnString.toString();
+    }
+
+    private HttpEntity getEntity(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("x-rapidapi-host", "systran-systran-platform-for-language-processing-v1.p.rapidapi.com");
+        headers.add("x-rapidapi-key",  "hiVJaOl7eamsh6lVNbVigZtUYm2up1AJm9Qjsn2pV7pyFHLVjC");
+        headers.add("accept", "application/json");
+
+        return new HttpEntity(headers);
+    }
+
+    class ResponseList {
+        private List<SpanishResponse> outputs;
+    }
+
+    class SpanishResponse {
+        private String output;
+        Object stats;
+
     }
 }
